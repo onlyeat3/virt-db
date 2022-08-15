e802a71d4743f04106c6139ad96a3c89f5b54db2
onlyeat3 <onlyeat3@gmail.com>
2022-08-15 11:24:33 +0800

    fix single connection only

--- server/src/server.rs
+++ server/src/server.rs
@@ -12,21 +12,15 @@ pub async fn start() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:3307").await?;
     loop{
         let (stream, _) = listener.accept().await?;
-        //TODO pool
-        let url = "mysql://root:root@127.0.0.1:3306";
-        let conn = Conn::from_url(url).await?;
-        let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-        let redis_conn = client.get_async_connection().await?;
+        tokio::spawn(async move {
+            //TODO pool
+            let url = "mysql://root:root@127.0.0.1:3306";
+            let real_mysql_conn = Conn::from_url(url).await.unwrap();
+            let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+            let redis_conn = client.get_async_connection().await.unwrap();
 
-        AsyncMysqlIntermediary::run_on(MySQL::new( conn,redis_conn), stream).await;
-        // tokio::spawn(async move {
-        //     //TODO pool
-        //     let url = "mysql://root:root@127.0.0.1:3306";
-        //     let conn = Conn::from_url(url).await?;
-        //     let client = redis::Client::open("redis://127.0.0.1/").unwrap();
-        //     let redis_conn = client.get_connection().unwrap();
-        //
-        //     AsyncMysqlIntermediary::run_on(MySQL::new( conn,redis_conn), stream).await
-        // });
+            let r = AsyncMysqlIntermediary::run_on(MySQL::new( real_mysql_conn,redis_conn), stream).await;
+            info!("mysql end result:{:?}",r);
+        });
     }
 }
