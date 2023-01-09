5a0f44d4e5663db2d61960efd5f128099f59c31a
onlyeat3 <onlyeat3@gmail.com>
2023-01-09 09:47:13 +0800

    feat: sqlx -> sea-orm

--- admin/src/controller/cache_config_controller.rs
+++ admin/src/controller/cache_config_controller.rs
@@ -1,16 +1,25 @@
 use actix_web::{get, HttpResponse, post, Responder, web};
-use actix_web::web::Data;
+use actix_web::web::{BufMut, Data};
+use sea_orm::{DatabaseConnection, entity, EntityTrait, PaginatorTrait, QueryFilter};
+use sea_orm::sea_query::{Expr, Query, SimpleExpr};
 use serde_json::json;
-use sqlx::{Database, MySql, MySqlPool, Pool, query};
+use sqlx::{Database, MySql, MySqlPool, Pool, query, QueryBuilder};
+use crate::entity::prelude::CacheConfig;
 
 use crate::error::SysError;
 use crate::model::{cache_config_model, CurrentUser, DataWrapper, PageResponse};
 use crate::model::cache_config_model::{CacheConfigEntity, CacheConfigListParam};
 
 #[post("/cache_config/list")]
-pub async fn list(req:web::Json<CacheConfigListParam>,pool: Data<MySqlPool>,current_user: CurrentUser) -> Result<HttpResponse, SysError> {
+pub async fn list(req:web::Json<CacheConfigListParam>, conn: Data<DatabaseConnection>, current_user: CurrentUser) -> Result<HttpResponse, SysError> {
+    // let query = CacheConfig::find();
+    // if req.cache_name.is_some() && !req.cache_name?.is_empty(){
+    //     query.filter(Expr::col(entity::Column::).is_null())
+    // }
+    // query.paginate(conn.get_ref(), req.page_param.page_size as u64)
+    //     .limit(req.page_param.page_size as usize);
     let cache_config_list_result = sqlx::query_as::<_, CacheConfigEntity>("SELECT * from cache_config")
-        .fetch_all(pool.get_ref())
+        .fetch_all(conn.get_ref())
         .await
         .map_err(anyhow::Error::new)?;
 
--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::time::Duration;
 use actix_cors::Cors;
 use actix_settings::{ApplySettings as _, BasicSettings};
 use actix_web::{App, get, http, HttpServer, post, Responder, web};
@@ -6,6 +7,7 @@ use actix_web::http::header;
 use actix_web::middleware::{Compress, Condition, Logger};
 use env_logger::Env;
 use log::info;
+use sea_orm::{ConnectOptions, Database};
 use serde::{de, Deserialize};
 use sqlx::MySqlPool;
 use crate::config::app_config::ApplicationSettings;
@@ -16,6 +18,7 @@ mod model;
 mod utils;
 mod config;
 mod error;
+mod entity;
 
 
 #[actix_web::main] // or #[tokio::main]
@@ -24,7 +27,17 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     let settings: BasicSettings<ApplicationSettings> = config::app_config::load_config();
     env::set_var("DATABASE_URL",settings.application.mysql_url.as_str());
-    let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap();
+    let mut opt = ConnectOptions::new(settings.application.mysql_url.as_str().to_owned());
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Info); // Setting default PostgreSQL schema
+
+    let db = Database::connect(opt).await?;
 
     let http_server = HttpServer::new({
         let settings = settings.clone();
@@ -38,7 +51,7 @@ async fn main() -> std::io::Result<()> {
                     Compress::default(),
                 ))
                 .app_data(web::Data::new(settings.clone()))
-                .app_data(web::Data::new(pool.clone()))
+                .app_data(web::Data::new(db))
 
                 .wrap(Cors::default()
                     .allowed_origin("http://localhost:8848")
