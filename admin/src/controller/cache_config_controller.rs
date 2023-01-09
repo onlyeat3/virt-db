556c8067746a3a806c6ccea863b898ab45fea4a4
onlyeat3 <onlyeat3@gmail.com>
2023-01-09 14:59:33 +0800

    fix: Convert seconds 86400 to FixedOffset fail

--- admin/src/controller/cache_config_controller.rs
+++ admin/src/controller/cache_config_controller.rs
@@ -3,8 +3,10 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use actix_web::{get, HttpResponse, post, Responder, web};
 use actix_web::web::{BufMut, Data};
+use anyhow::Error;
 use log::info;
-use sea_orm::{DatabaseConnection, entity, EntityTrait, PaginatorTrait, QueryFilter, QuerySelect, Select};
+use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection, entity, EntityTrait, NotSet, PaginatorTrait, QueryFilter, QuerySelect, Select};
+use sea_orm::ActiveValue::Set;
 use sea_orm::sea_query::{Expr, Query, SimpleExpr};
 use serde_json::json;
 use crate::AppState;
@@ -14,7 +16,7 @@ use crate::entity::prelude::CacheConfig;
 
 use crate::error::SysError;
 use crate::model::{cache_config_model, CurrentUser, DataWrapper, PageResponse};
-use crate::model::cache_config_model::{CacheConfigListParam};
+use crate::model::cache_config_model::{CacheConfigCreateParam, CacheConfigListParam};
 
 #[post("/cache_config/list")]
 pub async fn list(req: web::Json<CacheConfigListParam>, app_state: Data<AppState>, current_user: CurrentUser) -> Result<HttpResponse,SysError> {
@@ -45,6 +47,25 @@ pub async fn list(req: web::Json<CacheConfigListParam>, app_state: Data<AppState
         list,
         total: items_and_page_number.number_of_items as i64,
     });
+    Ok(HttpResponse::Ok()
+        .json(data_wrapper))
+}
+
+#[post("/cache_config/createOrUpdate")]
+pub async fn create(req: web::Json<cache_config::Model>, app_state: Data<AppState>, current_user: CurrentUser) -> Result<HttpResponse,SysError> {
+    let conn = &app_state.conn;
+    let cache_config_entity = cache_config::ActiveModel{
+        id: Set(req.id.to_owned()),
+        sql_template: Set(req.sql_template.to_owned()),
+        duration: Set(req.duration.to_owned()),
+        cache_name: Set(req.cache_name.to_owned()),
+        remark: Set(req.remark.to_owned()),
+        enabled: Set(req.enabled.to_owned()),
+        created_by: Set(current_user.user_id as i64),
+        ..Default::default()
+    };
+    cache_config_entity.save(conn);
+    let data_wrapper = DataWrapper::success("");
     Ok(HttpResponse::Ok()
         .json(data_wrapper))
 }
