819d428adc43941c12f67fa23033a962925c5c95
onlyeat3 <onlyeat3@gmail.com>
2023-01-10 13:00:51 +0800

    feat: 不再使用sqlx加载缓存配置，改用mysql_async自带API

--- server/src/meta.rs
+++ server/src/meta.rs
@@ -7,7 +7,9 @@ use tokio::{spawn, time};
 use crate::sys_config::VirtDBConfig;
 use log::{info};
 use mysql_async::{Conn, Opts, QueryResult, TextProtocol};
-use mysql_async::prelude::Queryable;
+use mysql_async::prelude::{Query, Queryable, WithParams};
+use mysql_common::frunk::hlist_pat;
+use mysql_common::row::Row;
 use sqlx::mysql::MySqlPoolOptions;
 
 static mut CACHE_CONFIG_ENTITY_LIST: Vec<CacheConfigEntity> = vec![];
@@ -50,17 +52,24 @@ pub async fn enable_meta_refresh_job(sys_config: VirtDBConfig) {
         let meta_mysql_port = meta_config.port;
         let meta_mysql_database = meta_config.database;
         let mysql_url = format!("mysql://{}:{}@{}:{}/{}", meta_mysql_username, meta_mysql_password, meta_mysql_ip, meta_mysql_port, meta_mysql_database).clone();
-        let pool = MySqlPoolOptions::new()
-            .max_connections(1)
-            .connect(&mysql_url).await.unwrap();
-        let sql = "select * from cache_config where enabled = true";
-        loop {
-            let cache_config_list = sqlx::query_as::<_, CacheConfigEntity>(sql)
-                .fetch_all(&pool)
-                .await.unwrap();
-            set_cache_config_entity_list(cache_config_list);
-            // info!("cache_config_list:{:?}", get_cache_config_entity_list());
+        let mut conn = Conn::from_url(mysql_url).await.unwrap();
 
+        loop {
+            let cache_config_list = "select id,sql_template,duration from cache_config where enabled = true"
+                .with(())
+                .map(&mut conn, |(id,sql_template,duration)| CacheConfigEntity {
+                    id,
+                    sql_template,
+                    duration,
+                    cache_name: "".to_string(),
+                    remark: "".to_string(),
+                    enabled: -1,
+                    created_by: -1,
+                    updated_by: -1,
+                }).await
+                .unwrap();
+            set_cache_config_entity_list(cache_config_list);
+            debug!("reload cache_config_list finish");
 
             time::sleep(Duration::from_secs(10)).await;
         }
